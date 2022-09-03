78d3f68b355425299836800419d80d067dfb8b24
shadik <shadikmahamudsohan@gmail.com>
2022-09-03 20:00:53 +0600

    boilar plate code added

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const dbConnect = require("./utils/dbConnect");
 const toolsRoutes = require("./routes/v1/tools.route.js");
 const errorHandler = require("./middleware/errorHandler");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
-app.set("view engine", "ejs");
 
-// app.use(viewCount);
-
-
-// Apply the rate limiting middleware to all requests
-// app.use(limiter);
-
-dbConnect();
 
 app.use("/api/v1/tools", toolsRoutes);
 
 app.get("/", (req, res) => {
-  // res.send("Hello World");
-  // res.sendFile(__dirname + "/public/test.html");
-  res.render("home.ejs",{
-    id: 5,
-    user: {
-      name: "test"
-    }
-  });
+  res.send("Find random users");
 });
 
 app.all("*", (req, res) => {
